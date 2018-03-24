bf1b0254b30c8695c9ec9e77a8df17cdd89f1af1
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-03-24 23:20:51 +0100

    Adding Keyboard Shortcuts

---
+++ Aura Operating System/Aura OS/Core/KeyboardShortcuts.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IL2CPU.API.Attribs;
+using Cosmos.System.ScanMaps;
+using Cosmos.System;
+using Cosmos.HAL;
+using Aura = System;
+using Aura_OS.System.Utils;
+
+namespace Aura_OS.Core
+{
+    [Plug(Target = typeof(Cosmos.System.KeyboardManager))]
+
+    public static class KeyboardManager
+    {
+
+        public static bool NumLock
+        {
+            get;
+            set;
+        }
+
+        public static bool CapsLock
+        {
+            get;
+            set;
+        }
+
+        public static bool ScrollLock
+        {
+            get;
+            set;
+        }
+
+        public static bool ControlPressed
+        {
+            get;
+            set;
+        }
+
+        public static bool ShiftPressed
+        {
+            get;
+            set;
+        }
+
+        public static bool AltPressed
+        {
+            get;
+            set;
+        }
+
+
+        public static bool KeyAvailable
+        {
+            get
+            {
+                return mQueuedKeys.Count > 0;
+            }
+        }
+
+        public static List<KeyboardBase> Keyboards = new List<KeyboardBase>();
+
+        private static ScanMapBase _scanMap = new US_Standard();
+        private static Queue<KeyEvent> mQueuedKeys = new Queue<KeyEvent>();
+
+        private static void Enqueue(KeyEvent keyEvent)
+        {
+            mQueuedKeys.Enqueue(keyEvent);
+        }
+
+        /// <summary>
+        /// Allow faking scancodes. Used for test kernels
+        /// </summary>
+        internal static void HandleFakeScanCode(byte aScancode, bool aReleased)
+        {
+            HandleScanCode(aScancode, aReleased);
+        }
+
+        private static void HandleScanCode(byte aScanCode, bool aReleased)
+        {
+            byte key = aScanCode;
+            if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.CapsLock) && !aReleased)
+            {
+                // caps lock
+                CapsLock = !CapsLock;
+                UpdateLeds();
+            }
+            else if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.NumLock) && !aReleased)
+            {
+                // num lock
+                NumLock = !NumLock;
+                UpdateLeds();
+            }
+            else if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.ScrollLock) && !aReleased)
+            {
+                // scroll lock
+                ScrollLock = !ScrollLock;
+                UpdateLeds();
+            }
+            else if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.LCtrl) || _scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.RCtrl))
+            {
+                ControlPressed = !aReleased;
+            }
+            else if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.LShift) || _scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.RShift))
+            {
+                ShiftPressed = !aReleased;
+            }
+            else if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.LAlt) || _scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.RAlt))
+            {
+                AltPressed = !aReleased;
+            }
+            else
+            {
+                if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.UpArrow) && !aReleased)
+                {
+                    //Global.Console.WriteLine("Detected Ctrl-Alt-Delete! Rebooting System...");
+                    CommandsHistory.ClearCurrentConsoleLine();
+                    Kernel.BeforeCommand();
+                    string cmd = CommandsHistory.GetUpCommand(CommandsHistory.GetIndex());
+                    Aura.Console.Write(cmd);
+                    ConsoleKeyInfo cmdkey = Aura.Console.ReadKey();
+                    if (cmdkey.Key == ConsoleKey.Enter)
+                    {
+                        Shell.cmdIntr.CommandManager._CommandManger(cmd);
+                    }
+                }
+
+                if (_scanMap.ScanCodeMatchesKey(key, ConsoleKeyEx.DownArrow) && !aReleased)
+                {
+                    //Global.Console.WriteLine("Detected Ctrl-Alt-Delete! Rebooting System...");
+                    CommandsHistory.ClearCurrentConsoleLine();
+                    Kernel.BeforeCommand();
+                    string cmd = CommandsHistory.GetUpCommand(CommandsHistory.GetIndex());
+                    Aura.Console.Write(cmd);
+                    ConsoleKeyInfo cmdkey = Aura.Console.ReadKey();
+                    if (cmdkey.Key == ConsoleKey.Enter)
+                    {
+                        Shell.cmdIntr.CommandManager._CommandManger(cmd);
+                    }
+                }
+
+                if (!aReleased)
+                {
+                    KeyEvent keyInfo;
+
+                    if (GetKey(key, out keyInfo))
+                    {
+                        Enqueue(keyInfo);
+                    }
+                }
+            }
+        }
+
+        private static void UpdateLeds()
+        {
+            foreach (KeyboardBase keyboard in Keyboards)
+            {
+                keyboard.UpdateLeds();
+            }
+        }
+
+        public static bool GetKey(byte aScancode, out KeyEvent keyInfo)
+        {
+            if (_scanMap == null)
+            {
+                //Global.mDebugger.Send("No KeyLayout");
+            }
+
+            keyInfo = _scanMap.ConvertScanCode(aScancode, ControlPressed, ShiftPressed, AltPressed, NumLock, CapsLock, ScrollLock);
+
+            return keyInfo != null;
+        }
+
+        public static bool TryReadKey(out KeyEvent oKey)
+        {
+            if (mQueuedKeys.Count > 0)
+            {
+                oKey = mQueuedKeys.Dequeue();
+                return true;
+            }
+
+            oKey = default(KeyEvent);
+
+            return false;
+        }
+
+        public static KeyEvent ReadKey()
+        {
+            while (mQueuedKeys.Count == 0)
+            {
+                KeyboardBase.WaitForKey();
+            }
+
+            return mQueuedKeys.Dequeue();
+        }
+
+        public static ScanMapBase GetKeyLayout()
+        {
+            return _scanMap;
+        }
+
+        public static void SetKeyLayout(ScanMapBase ScanMap)
+        {
+            if (ScanMap != null)
+            {
+                _scanMap = ScanMap;
+            }
+        }
+
+        public static void AddKeyboard(KeyboardBase Keyboard)
+        {
+            //if (!KeyboardExists(Keyboard.GetType()))
+            //{
+            Keyboard.OnKeyPressed = HandleScanCode;
+            Keyboards.Add(Keyboard);
+            //}
+        }
+
+        //public static bool KeyboardExists(Type KeyboardType)
+        //{
+        //    foreach (KeyboardBase Keyboard in Keyboards)
+        //    {
+        //        if (Keyboard.GetType() == KeyboardType)
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+    }
+}
